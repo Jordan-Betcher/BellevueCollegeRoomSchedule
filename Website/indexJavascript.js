e94bc99818052efa912d9055b001ef9c42845164
Plum <pplluumm@users.noreply.github.com>
2019-09-05 13:52:31 -0700

    Working on API

--- Website/indexJavascript.js
+++ Website/indexJavascript.js
@@ -19,6 +19,22 @@ function search()
     let building = room[0];
     let roomNumber = room.substring(1, 4);
 
+    //https://67gi99ndv2.execute-api.us-west-2.amazonaws.com/Publish/room?BuildingLetter=L&RoomNumber=219&Day=Monday
+    let api = `https://67gi99ndv2.execute-api.us-west-2.amazonaws.com/Publish/room`
+    + `?BuildingLetter=${building}`
+    + `&RoomNumber=${roomNumber}`
+    + `&Day=${day}`;
+
+    console.log("fetching");
+    fetch(api)
+        .then(function(res)
+        {
+            console.log(res);
+        });
+    console.log("done fetching");
+
+    console.log(api);
+
     console.log(building);
     console.log(roomNumber);
     console.log(room);
