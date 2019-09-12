464339ec460750e20407f6de9d55e75a01a5692a
Plum <pplluumm@users.noreply.github.com>
2019-09-12 15:28:57 -0700

    Creates and loads from url api

--- Website/roomScheduleJavascript.js
+++ Website/roomScheduleJavascript.js
@@ -1,5 +1,3 @@
-
-
 function search()
 {
     event.preventDefault();
@@ -46,11 +44,32 @@ function search()
         )
     console.log("done fetching");
 
+    changeURL(room, day);
+}
 
-    console.log(building);
-    console.log(roomNumber);
-    console.log(room);
-    console.log(day);
+function changeURL(room, day)
+{
+    let url = document.URL;
+    let addOns = `room=${room}&day=${day}`;
+
+    console.log("changine url function");
+    console.log(url.search("room"));
+
+    if(url.search(addOns) != -1)
+    {
+        //skip
+        return;
+    }
+    else if(url.search("room=") != -1)
+    {
+        //have a different room than is there
+        url = url.split("?")[0];
+        window.history.pushState("", "", url + "?" + addOns);
+    }
+    else
+    {
+        window.history.pushState("", "", url + "?" + addOns);
+    }
 }
 
 const increments = 30;
@@ -130,3 +149,40 @@ function createClasstimeTable(classTimeJSON)
         `
     });//*/
 }
+
+function main()
+{
+    console.log("main");
+    let url = document.URL;
+    if(url.search("room=") != -1)
+    {
+        console.log("room found");
+        let roomParam = url.split("?")[1].split("&")[0];
+        let dayParam = url.split("?")[1].split("&")[1];
+        
+        if(roomParam == undefined)
+        {
+            return;
+        }
+        
+        let room = roomParam.split("=")[1];
+        document.forms["searchForm"]["room"].value = room;
+
+        if(dayParam == undefined)
+        {
+            return;
+        }
+
+        let day = dayParam.split("=")[1];
+        document.forms["searchForm"]["day"].value = day;
+
+        if(room != undefined && day != undefined)
+        {
+            search();
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    main();
+})
